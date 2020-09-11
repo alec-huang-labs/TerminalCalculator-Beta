fa95236e1a2d60f514ba5f593b5faf742a48100c
Alec Huang <ahsihuang@gmail.com>
2020-09-10 20:20:45 -0400

    Updates

---
+++ precache-manifest.068c3ceed23b3398dc78f2062e40c39d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ad1beb95e0e5f8448a8eb36af19a42b",
+    "url": "/TerminalCalculator-Beta/index.html"
+  },
+  {
+    "revision": "f943980399a35d61bc13",
+    "url": "/TerminalCalculator-Beta/static/css/main.2e616e62.chunk.css"
+  },
+  {
+    "revision": "6a0d3d16d791a66636d6",
+    "url": "/TerminalCalculator-Beta/static/js/2.292230ea.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/TerminalCalculator-Beta/static/js/2.292230ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f943980399a35d61bc13",
+    "url": "/TerminalCalculator-Beta/static/js/main.0c6733e8.chunk.js"
+  },
+  {
+    "revision": "dd32c75e07850a611262",
+    "url": "/TerminalCalculator-Beta/static/js/runtime-main.711324cf.js"
+  }
+]);
